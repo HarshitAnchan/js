8e0d486d3d9b11b1e5704c5b21efac096b564ab2
harshitanchan <harshitanchan647@gmail.com>
2024-03-23 13:36:44 +0530

    datatypes done

---
+++ basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs , not browser
+
+console.log(3+
+    3)  // code readability should be high
+
+console.log("harshit")
+
+
+let name = "harshit"
+let age = 18
+let isLoggedIn= true
+
+//Datatypes
+
+
+//number => 2 to pwer 53 (range)
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value 
+//undefined => 
+//symbol => unique 
+
+
+
+
+
+//Object 
+
+console.log(typeof undefined);
