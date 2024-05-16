6d4c1a991bbab84dd334e0b80538d3afe03d6799
harshitanchan <harshitanchan647@gmail.com>
2024-05-16 21:47:42 +0530

    conversion of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = true
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1, false = >0
+
+let isLoggedIn = 0
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true ,  0 => false
+
+// "" => false --- empty string
+
+// "Harshit" => true
+
+
+let sumNumber =33
+
+let stringNumber = String(sumNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
