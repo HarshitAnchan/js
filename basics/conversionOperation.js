45e34655e5835eded9162103da0e46ee43dd5028
harshitanchan <harshitanchan647@gmail.com>
2024-05-17 11:35:33 +0530

    postfix and prefix increment

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -14,7 +14,7 @@ let isLoggedIn = 0
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 
 // 1 => true ,  0 => false
@@ -24,10 +24,46 @@ console.log(booleanIsLoggedIn)
 // "Harshit" => true
 
 
-let sumNumber =33
+ let sumNumber =33
 
 let stringNumber = String(sumNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
+//************************ OPERATION *************************/
+
+let value =3
+let negValue=-value 
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2=" harshit"
+
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2 );
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1+2+"2");
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+
+
+let  x = 4 
+let y= x++
+console.log(`x:${x}, y:${y}`);
+
+let a = 4
+let b= ++a
+console.log(`a:${a}, b:${b}`);
 
