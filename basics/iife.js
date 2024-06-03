6e6899e058f6027f2b192701ededae35a3769e70
harshitanchan <harshitanchan647@gmail.com>
2024-06-03 11:06:38 +0530

    iife

---
+++ basics/iife.js
@@ -0,0 +1,20 @@
+// Immediately Invoked Function Expression (II
+
+(function chai (){
+  console.log(`Db connected`);
+
+})();
+
+
+( (name) =>{
+
+        console.log(`Db Connected2 ${name}`);
+    }
+)('harshit');
+
+
+
+
+
+
+
