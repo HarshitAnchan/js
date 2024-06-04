6bca6065e59b5d2f1d7e3336056f4f40e7912b2a
harshitanchan <harshitanchan647@gmail.com>
2024-06-04 12:18:00 +0530

    nested

--- basics/one.js
+++ basics/one.js
@@ -13,19 +13,21 @@ const temperature = 41
 
 
 
-const score = 200
+// const score = 200
 
-if (score > 100) {
-    let power = "fly"
-    console.log(`User power: ${power}`);
-}
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
 
-console.log(`User power: ${power}`);
+// console.log(`User power: ${power}`);
+
+
+
 
 
 // const balance = 1000
 
-// if (balance > 500) console.log("test"),console.log("test2");
 
 // if (balance < 500) {
 //     console.log("less than 500");
