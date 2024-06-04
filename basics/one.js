23ae5e38956594f69f965367759f022c3481b648
harshitanchan <harshitanchan647@gmail.com>
2024-06-04 12:39:35 +0530

    controlflow

--- basics/one.js
+++ basics/one.js
@@ -42,15 +42,18 @@ const temperature = 41
 
 // }
 
-// const userLoggedIn = true
-// const debitCard = true
-// const loggedInFromGoogle = false
-// const loggedInFromEmail = true
 
-// if (userLoggedIn && debitCard && 2==3) {
-//     console.log("Allow to buy course");
-// }
 
-// if (loggedInFromGoogle || loggedInFromEmail) {
-//     console.log("User logged in");
-// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
