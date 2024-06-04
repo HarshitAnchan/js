107880ebc49e141cc04c9fc6e645f7dbd0f994f0
harshitanchan <harshitanchan647@gmail.com>
2024-06-04 11:15:21 +0530

    if else

---
+++ basics/one.js
@@ -0,0 +1,28 @@
+// if 
+
+// const isUserloggedIn = true
+
+// const temperature = 41
+
+// if(temperature <50 ){
+//     console.log("less tha 50");
+// }else{
+
+//     console.log("temp is greater than 50")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
