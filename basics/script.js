fb0d1963d884abd3fcd098d649689d49978f0262
harshitanchan <harshitanchan647@gmail.com>
2024-06-13 21:58:03 +0530

    practice Ques

--- basics/script.js
+++ basics/script.js
@@ -31,18 +31,44 @@
 
 
 
-let age = 20;
+// let age = 20;
 
-switch (age) {
-    case age < 18:
-        console.log("under 18");
-        break;
-    case age >=18:
-        console.log("above 18");
-        break;
+// switch (age) {
+//     case age < 18:
+//         console.log("under 18");
+//         break;
+//     case age >=18:
+//         console.log("above 18");
+//         break;
 
-    default:
-        console.log("hello idk your age");
-        break;
+//     default:
+//         console.log("hello idk your age");
+//         break;
     
+// }
+
+
+// let num = prompt("Enter Your number");
+
+// if (num %5 == 0 ) {
+//     console.log(`the num ${num} is multiple of 5`);
+// }else{
+//     console.log(`it is not the multiple of 5`);
+// }
+
+
+
+let score = prompt("ENTER SCORE");
+
+if (score >=80 && score<=100) {
+    console.log("A grade");
+} else if (score ==70 && score<=79) {
+    console.log("B grade");
+}else if (score==60 && score<=69) {
+    
+}else if (score ==50  && score<=59) {
+    
+}else{
+
+console.log("F grade");
 }
