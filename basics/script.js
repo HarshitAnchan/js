47698fc16d90672681241e09007959a5a646947f
harshitanchan <harshitanchan647@gmail.com>
2024-06-13 21:16:56 +0530

    conditionals

--- basics/script.js
+++ basics/script.js
@@ -1,4 +1,25 @@
-let a =5;
-let b=2;
-a--;
-console.log(a)
+// int age = 19;
+
+// if(age >= 18){
+//     console.log("Can have license");
+
+// }else{
+//     console.log("Yoou cannot have license");
+// }
+
+// let num = 10;
+// if (num %2 ===  0) {
+//     console.log(num, "is even");
+// }else{
+//     console.log(num, "is odd");
+// }
+
+let mode = "yellow";
+let color;
+if (mode == "dark") {
+    console.log("black");
+}else if (mode === "blue") {
+    console.log("blue");
+} else {
+    console.log(color = "white");
+}
