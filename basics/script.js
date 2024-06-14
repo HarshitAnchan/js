238fe2f4b5556d587f9ce2a1bda253276379bfb9
harshitanchan <harshitanchan647@gmail.com>
2024-06-14 10:38:00 +0530

    while

--- basics/script.js
+++ basics/script.js
@@ -82,12 +82,23 @@
 
 // Calculate sum of 1 to n
 
-let sum=0;
-let n =100
-for(let i = 1; i <= n; i++ ){
-    sum = sum + i
-}
-console.log(`sum = ${sum}`);
+// let sum=0;
+// let n =100
+// for(let i = 1; i <= n; i++ ){
+//     sum = sum + i
+// }
+// console.log(`sum = ${sum}`);
+
+
+
+// let i =1;
+// while (i<=5) {
+//     console.log(i);
+//         i++;
+// }
+
+
+
 
 
 
